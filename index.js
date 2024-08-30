eb4112c119ed3dbde71ce5b1c122179a459d57ba
Renata Pinheiro <133382910+renatapinheirobueno@users.noreply.github.com>
2024-08-30 14:38:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const primeiroNome = "Renata";
+const sobrenome = "Pinheiro";
+const idade = 16;
+const altura = 1.60;
+const massa = 56;
+const imc = massa / (altura * altura);
+
+console.log(`Paciente ${primeiroNome} ${sobrenome} tem idade ${idade}, altura ${altura}, massa ${massa} e seu imc é ${imc}`);
